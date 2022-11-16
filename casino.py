6d3b5962fdb4f930985304439a0978d4dbf2d07b
GeshG <georgi.ginchev@gmail.com>
2022-11-17 00:30:07 +0200

    Add files via upload

--- casino.py
+++ casino.py
@@ -1,68 +1,169 @@
-from random import random
-
-
-class Casino:
-    def __init__(self, available_capacity):
-        self.available_capacity = available_capacity
-
-    def capacity(self, players):
-        player_count = []
-        for player in players:
-            if self.available_capacity > len(players):
-                player_count.append(player)
-        return (f'Curret count of players is {len(player_count)} out of max capacity {self.available_capacity}.')
+import random
 
 class Player:
-    def __init__(self, name, player_roll, player_balance):
+    def __init__(self, name):
         self.name = name
-        self.player_roll = player_roll
-        self.player_balance = player_balance
+    
+    def player_money(self):
+        self.money = 100
+        return self.money
 
-    def welcome(self):
-        return (f'Welcome to Casino Royal {self.name}!')
+class Roulette:
+    def __init__(self, balance):
+        self.balance = balance
 
-    def player_roll_rnd(self):
-        return self.player_roll
-
-    def player_balances(self):
-        return (f'Your current balance is {self.player_balance}$.')
-
-class Games:
-
-    casino_balance = 100
-
-    def __init__(self, casino_roll):
-        self.casino_roll = casino_roll
-
-    def game_wins(self, player):
-        b_change = 0
-        if self.casino_roll > player.player_roll_rnd():
-            self.casino_balance += 10
-            b_change = player.player_balance - 10
-        return (f'Casino balance has increased with {self.casino_balance}$. {player.name} balance has decreased with {b_change}$')
+    def place_bet(self, player):
+        if player.player_money() > 0:
+            print('You can continue betting.')
+        else:
+            print('You\'ll need to go to the ATM')
         
-    def game_loses(self, player):
-        game_balance = 50
-        if  self.casino_roll < player.player_roll_rnd():
-            game_balance -= 10
-            player.player_balance +=10
-        return (f'Game balance change: {game_balance}. {player.name} balance has changed to {player.player_balance}')
+    def roulette_numbers(self, player):
+        bet_money = int(input("How much you would like to bet? "))
+        pick_number = int(input('Please, pick a number: '))
+        casino_rnd = random.choice([0,1,2,3,4,5,6,7,8,9,10,11,12])
+        red = 'red'
+        black = 'black'
+        while True:
+            if pick_number >= 0 and pick_number <= 12:
+                if pick_number == 0 and casino_rnd == 0:
+                    print(f'The number is {pick_number}')
+                    print(f'The casino number is {casino_rnd}')
+                    print('You win')
+                elif pick_number == 0 and casino_rnd != 0:
+                    print(f'The number is {pick_number}')
+                    print(f'The casino number is {casino_rnd}')
+                    print('You lose')
+                else:
+                    if pick_number % 2 == 0:
+                        print(f'Your number is {pick_number}')
+                        print(f'The colour of number {pick_number} is {black}')
+                        print(f'The casino number is {casino_rnd}')
+                        if casino_rnd % 2 == 0:
+                            print(f'The color of the casino number {casino_rnd} is {black}')
+                        else:
+                            print(f'The color of the casino number {casino_rnd} is {red}')
+                        if pick_number == casino_rnd:
+                            print('You win')
+                            winner = bet_money * 1.5
+                            new_amount = winner + player.player_money()
+                            print(f'You win ${winner}')
+                            print(f'New balance {new_amount}')
+                        else:
+                            print('You lose')
+                            losing_money = player.player_money() - bet_money
+                            print(f'New balance {losing_money}')
+                    else:
+                        print(f'Your number is {pick_number}')
+                        print(f'The color of number {pick_number} is {red}')
+                        print(f'The casino number is {casino_rnd}')
+                        if casino_rnd % 3 == 0:
+                            print(f'The color of the casino number {casino_rnd} is {red}')
+                        else:
+                            print(f'The color of the casino number {casino_rnd} is {black}')
+                        if pick_number == casino_rnd:
+                            print('You win')
+                            winner = bet_money * 1.75
+                            new_amount = winner + player.player_money()
+                            print(f'You win ${winner}')
+                            print(f'New balance {new_amount}')
+                        else:
+                            print('You lose')
+                            losing_money = player.player_money() - bet_money
+                            print(f'New balance {losing_money}')
+            else:
+                    print('Please, enter a number between 0 and 12')
+            print('Do you want to continue playing?')
+            play_more = input('Yes / No: ')
+            if play_more == 'Yes'.lower():
+                pick_number = int(input('Please, pick a number: '))
+            else:
+                print('Thank you for playing! GoodBye!')
+                break
+
+class Slots:
+    symbols = ['@','#','$']
+    def __init__(self) -> None:
+        pass
+    
+    def play_slots(self):
         
+        player_money = 100
+        player_money_bet = 0
+        player_money_new_balance = 0
+        print(f'You have, {player_money} dollars.')
+        try:
+            bet = int(input('Bet amount: '))
+        except:
+            print('Please, enter a whole number: ')
+        while True:
+            if player_money <= 0:
+                print('Not enough money')
+                print('Please, visit the nearest ATM')
+                break
+            else:
+                symbol_one = random.choice(self.symbols)
+                symbol_two = random.choice(self.symbols)
+                symbol_three = random.choice(self.symbols)
+                        
+                print('\n|',random.choice(self.symbols), '|', random.choice(self.symbols), '|', random.choice(self.symbols), '|')
+                print(13*'-')
+                print('|', symbol_one,'|', symbol_two, '|', symbol_three, '|')
+                print(13*'-')
+                print('|',random.choice(self.symbols), '|', random.choice(self.symbols), '|', random.choice(self.symbols), '|\n')
 
+                if symbol_one == symbol_two and symbol_two == symbol_three:
+                    player_money_bet = bet*1.7
+                    print(f'You have bet {bet} x 2 = {player_money_bet}')
+                    print(f'You won {player_money_bet}')
+                    player_money_new_balance = player_money_bet + player_money
+                    print(f'Your new balance is {player_money_new_balance}')
+                else:
+                    print('You lost this time')
+                    player_money_new_balance = player_money - player_money_bet
+                    print(f'You have lost {player_money_bet}')
+                    print(f'Your new balace is {player_money_new_balance}')
+                    if player_money_new_balance <=0:
+                        print('You have lost. Not enough money to continue.')
+                        break
+            print('Do you want to continue playing?')
+            play_more = input('Yes / No: ')
+            if play_more == 'Yes'.lower():
+                bet = int(input('Bet amount: '))
+            else:
+                print('Thank you for playing! GoodBye!')
+                break
 
+class Play:
+    @classmethod
+    def run(cls):
 
- 
+        player = Player('Tommy')
+        roulette = Roulette(200)
+        slots = Slots()
 
-player1 = Player('Shaun', random(), 100)
-player2 = Player('Jim',random(), 200)
-print(player1.welcome())
-print(player1.player_balances())
-game1 = Games(random())
-print(game1.game_wins(player1))
-print(game1.game_loses(player1)) 
-cas = Casino(10)
-print(cas.capacity(['John', 'Jane', 'Pete']))
+        active = True
+        while active:
+            print('\nWELCOME TO CASINO ROYAL')
+            instructions = print(""" 
+            MAY THE LUCK BE WITH YOU.
 
+            Please, select from the following options to proceed:
 
+            1. Roulette
+            2. Slot Machine
+            3. Exit the Casino
 
+        """)
+            select_game = input('Make your selection: ')
+            select_game.isnumeric
+            select_game = int(select_game)
+            if select_game == 1:
+                roulette.roulette_numbers(player)
+            elif select_game == 2:
+                slots.play_slots()
+            elif select_game == 3:
+                print('Thank you for playing! GoodBye!')
+                break
 
+Play.run()
